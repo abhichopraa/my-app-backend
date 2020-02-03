94d88f50a23f1d553942807c74b64ba29018d321
Abhinav Chopra <abhinav.chopra@magicsw.com>
2020-02-03 11:10:26 +0530

    setup changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     var Schema = mongoose.Schema;
@@ -10,9 +13,34 @@ app.get('/', (req, res) => {
     MyModel.findOne(function (error, result) { 
         if(error){
             console.log(error)
+            res.send({
+                status: 404,
+                data: []
+            })
         }
-        res.send(result) });
-})
+        res.send({
+            status: 200,
+            data: result
+        }) });
+});
+
+app.post('/authenticateUser', (req, res) => {
+    var Schema = mongoose.Schema;
+    mongoose.connect('mongodb://localhost:27017/myAppDb')
+    var MyModel = mongoose.model('Test', new Schema({ email: String, password: String },{collection:'users'}));
+    MyModel.find(req.body,function (error, result) { 
+        if(error){
+            console.log(error)
+            res.send({
+                status: 404,
+                data: []
+            })
+        }
+        res.send({
+            status: 200,
+            data: result
+        }) });
+});
 
 app.listen(5000, () => {
     console.log('Server started on post 5000')
